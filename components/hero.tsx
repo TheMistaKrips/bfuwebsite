45028c6ddddbe20b88a77d3c6a0d9ec0c58cc900
KripsaKrips <144921464+TheMistaKrips@users.noreply.github.com>
2024-04-25 22:38:45 +0200

    Update hero.tsx

--- components/hero.tsx
+++ components/hero.tsx
@@ -8,9 +8,6 @@ export default function Hero() {
         <div className="relative pt-32 pb-10 md:pt-40 ">
           <div className="max-w-3xl mx-auto text-center pb-12 md:pb-16">
             <h1 className="h1 mb-4" data-aos="fade-up">Физмат</h1>
-            <div>
-              <Image src={bfu} data-aos="fade-up" />
-            </div>
             <p className="text-xl text-gray-400 " data-aos="fade-up" data-aos-delay="200">Коллекция двернейших предметов со своей историей</p>
           </div>
         </div>
